bebad62d670cb942aff5169ece7860a89d265ddb
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2016-05-25 17:51:17 +0200

    Improve runtime.

--- pygen.py
+++ pygen.py
@@ -9,6 +9,7 @@ from json import encoder, decoder
 import os.path
 import exceptions
 import sys
+import shutil
 import colorama
 
 LANG_EXT_C = ["c", "h"]
@@ -254,10 +255,8 @@ if __name__ == "__main__":
             print liststr
         choice = -1
         while choice < 0 or choice > len(projects):
-            print "Enter a valid project to build"
+            sys.stdout.write("Select a valid project: ")
             choice = int(sys.stdin.readline())
-
-        print "You chose " + str(choice) + ": " + os.path.relpath(projects[choice - 1], root)
 
         p = Project()
         p.parseUV(projects[choice - 1])
@@ -265,9 +264,50 @@ if __name__ == "__main__":
             print str(i + 1) + ": " + target.name
         choice = -1
         while choice < 0 or choice > len(p.targets):
-            print "Enter a valid target to build"
+            sys.stdout.write("Select a valid target: ")
             choice = int(sys.stdin.readline())
-        print "You chose " + str(choice) + ": " + p.targets[choice - 1].name
-        p.targets[choice - 1].build(ToolchainARMCC("C:\\Keil_v5\\ARM\\ARMCC\\"))
-        print 80 * "="
+        target = p.targets[choice - 1]
+        print target.name
+        prevchoice = -1
+        cont = True
+        while cont:
+            print 80 * "="
+            titlestr = "= " + p.name + " | " + target.name + " ="
+            titlestr += (80 - len(titlestr)) * "="
+            print titlestr
+            print 80 * "="
+            tasks = ["build", "clean", "print", "back", "quit"]
+            for (i, task) in enumerate(tasks):
+                print str(i + 1) + ": " + task
+            choice = -1
+            while choice < 0 or choice > len(tasks):
+                print "What do you want to do?"
+                instr = sys.stdin.readline()
+                if not instr or len(instr) is 1:
+                    choice = prevchoice
+                else:
+                    choice = int(instr)
+            prevchoice = choice
+            task = tasks[choice - 1]
+            if task == "build":
+                target.build(ToolchainARMCC("C:\\Keil_v5\\ARM\\ARMCC\\"))
+            elif task == "clean":
+                if not os.path.exists(os.path.join(target.cwd, target.outputdir)):
+                    print "Clean failed: Outputdir already cleaned."
+                else:
+                    try:
+                        for f in os.listdir(os.path.join(target.cwd, target.outputdir)):
+                            if f.endswith(".o") or f.endswith(".elf"):
+                                os.remove(os.path.join(target.cwd, target.outputdir, f))
+                        print "Successfully cleaned target " + target.name
+                    except OSError as e:
+                        print "Clean failed:"
+                        print e
+            elif task == "print":
+                print str(target)
+            elif task == "back":
+                print "Going back"
+                cont = False
+            elif task == "quit":
+                exit()
 
