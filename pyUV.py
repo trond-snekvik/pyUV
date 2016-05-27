1d36fe8b0857450012bcbee7388b3785dfeeb160
Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
2016-05-27 16:01:11 +0200

    Cleaner task selection

--- pyUV.py
+++ pyUV.py
@@ -1,8 +1,33 @@
 import pygen
+import subprocess
 import os
 import sys
 import colorama
 from toolchain_armcc import *
+
+def task_build(target):
+    target.build(ToolchainARMCC("C:\\Keil_v5\\ARM\\ARMCC\\"))
+
+def task_clean(target):
+    if not os.path.exists(os.path.join(target.cwd, target.outputdir)):
+        print "Clean failed: Outputdir already cleaned."
+    else:
+        try:
+            for f in os.listdir(os.path.join(target.cwd, target.outputdir)):
+                if f.endswith(".o") or f.endswith(".elf"):
+                    os.remove(os.path.join(target.cwd, target.outputdir, f))
+            print "Successfully cleaned target " + target.name
+        except OSError as e:
+            print "Clean failed:"
+            print e
+
+def task_print(target):
+    print str(target)
+
+def task_flash(target):
+    hexfile = os.path.join(target.cwd, target.outputdir, target.outputname + ".hex")
+    if os.path.exists(hexfile):
+
 
 if __name__ == "__main__":
     colorama.init()
@@ -43,11 +68,12 @@ if __name__ == "__main__":
             print titlestr
             print 80 * "="
             tasks = ["build", "clean", "print", "back", "quit"]
+            functions = {"build": task_build, "clean": task_clean, "print": task_print}
             for (i, task) in enumerate(tasks):
                 print str(i + 1) + ": " + task
             choice = -1
             while choice < 0 or choice > len(tasks):
-                print "What do you want to do?"
+                sys.stdout.write("What's next? ")
                 instr = sys.stdin.readline()
                 if not instr or len(instr) is 1:
                     choice = prevchoice
@@ -55,22 +81,8 @@ if __name__ == "__main__":
                     choice = int(instr)
             prevchoice = choice
             task = tasks[choice - 1]
-            if task == "build":
-                target.build(ToolchainARMCC("C:\\Keil_v5\\ARM\\ARMCC\\"))
-            elif task == "clean":
-                if not os.path.exists(os.path.join(target.cwd, target.outputdir)):
-                    print "Clean failed: Outputdir already cleaned."
-                else:
-                    try:
-                        for f in os.listdir(os.path.join(target.cwd, target.outputdir)):
-                            if f.endswith(".o") or f.endswith(".elf"):
-                                os.remove(os.path.join(target.cwd, target.outputdir, f))
-                        print "Successfully cleaned target " + target.name
-                    except OSError as e:
-                        print "Clean failed:"
-                        print e
-            elif task == "print":
-                print str(target)
+            if task in functions.keys():
+                functions[task](target)
             elif task == "back":
                 print "Going back"
                 cont = False
