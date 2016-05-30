5b8169272d0ae99d29013dc7fdab3d8738c2a922
Trond Einar Snekvik <trond.einar.snekvik@nordicsemi.no>
2016-05-30 22:37:06 +0200

    unify selection prompt

--- pyUV.py
+++ pyUV.py
@@ -65,6 +65,28 @@ def task_set_device(_):
         print("Filter is " + gFilter)
     store({"filter": gFilter})
 
+
+def select(optlist, query="", default = -1):
+    for (i, option) in enumerate(optlist):
+        if i == default:
+            liststr = ">"
+        else:
+            liststr = " "
+        liststr += str(i + 1) + ": " + option
+        print liststr
+    choice = -1
+    while choice < 0 or choice > len(optlist):
+        sys.stdout.write(query)
+        try:
+            choicestr = sys.stdin.readline()
+            if len(choicestr.splitlines()[0]) == 0 and default >= 0:
+                return default
+            choice = int(choicestr)
+        except ValueError:
+            choice = -1
+    return choice - 1
+
+
 if __name__ == "__main__":
     colorama.init()
     root = pygen.findroot(".")
@@ -85,6 +107,7 @@ if __name__ == "__main__":
                 gTarget = options["target"]
             if "filter" in options.keys():
                 gFilter = options["filter"]
+        print colorama.Fore.CYAN + 80 * "=" + colorama.Style.RESET_ALL
 
 
     projects = pygen.findUVprojects(root)
@@ -97,34 +120,19 @@ if __name__ == "__main__":
                 baselen = len(os.path.basename(proj).split(".")[0])
                 if baselen > maxlen:
                     maxlen = baselen
-            for (i,proj) in enumerate(projects):
-                liststr = str(i + 1) + ": " + os.path.basename(proj).split(".")[0]
+            projlist = []
+            for proj in projects:
+                liststr = os.path.basename(proj).split(".")[0]
                 liststr += (maxlen + 5 - len(liststr)) * " "
                 liststr += colorama.Fore.CYAN + "(" + os.path.relpath(proj, root) + ")" + colorama.Style.RESET_ALL
-                print liststr
-            choice = -1
-            while choice < 0 or choice > len(projects):
-                sys.stdout.write("Select a valid project: ")
-                try:
-                    choice = int(sys.stdin.readline())
-                except ValueError:
-                    choice = -1
-            gProject = projects[choice - 1]
+                projlist.append(liststr)
+            gProject = projects[select(projlist, "Select a valid project: ")]
             store({"project": gProject})
 
         p.parseUV(gProject)
 
         if gTarget == "" or not gTarget in [t.name for t in p.targets]:
-            for (i, target) in enumerate(p.targets):
-                print str(i + 1) + ": " + target.name
-            choice = -1
-            while choice < 0 or choice > len(p.targets):
-                sys.stdout.write("Select a valid target: ")
-                try:
-                    choice = int(sys.stdin.readline())
-                except ValueError:
-                    choice = -1
-            target = p.targets[choice - 1]
+            target = p.targets[select([target.name for target in p.targets], "Select a valid target: ")]
             gTarget = target.name
             store({"target": gTarget})
             print target.name
@@ -133,35 +141,24 @@ if __name__ == "__main__":
                 if gTarget == t.name:
                     target = t
                     break
-        prevchoice = -1
         cont = True
+        prevchoice = -1
         while cont:
             print colorama.Fore.CYAN + 80 * "="
             titlestr = "= " + p.name + " | " + target.name + " ="
             titlestr += (80 - len(titlestr)) * "="
             print titlestr
             print 80 * "=" + colorama.Style.RESET_ALL
+
             tasks = ["build", "clean", "print", "set device filter", "flash", "back", "quit"]
             functions = {"build": task_build, "clean": task_clean, "print": task_print, "flash": task_flash, "set device filter": task_set_device}
-            for (i, task) in enumerate(tasks):
-                print str(i + 1) + ": " + task
-            choice = -1
-            while choice < 0 or choice > len(tasks):
-                sys.stdout.write("What's next? ")
-                instr = sys.stdin.readline()
-                if not instr or len(instr) is 1:
-                    choice = prevchoice
-                else:
-                    try:
-                        choice = int(instr)
-                    except ValueError:
-                        choice = -1
-            prevchoice = choice
-            task = tasks[choice - 1]
+            prevchoice = select(tasks, "What's next? ", prevchoice)
+            task = tasks[prevchoice]
             if task in functions.keys():
                 functions[task](target)
             elif task == "back":
                 print "Going back"
+                print colorama.Fore.CYAN + 80 * "=" + colorama.Style.RESET_ALL
                 cont = False
                 gProject = ""
                 gTarget = ""
